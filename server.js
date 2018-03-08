a589148f6c97a1b4f936bf98c61418fe158fb09a
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-08 13:47:17 -0800

    testing heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// redirects https traffic to http for openWeatherMap
 app.use(function(req,res,next){
   if(req.headers['x-forwarded-proto'] === 'http'){
     next();
